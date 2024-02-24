cef0596c8b2b4622271d4c048a4dd9b1a1008380
Ordoga <ordogga@gmail.com>
2024-02-24 14:19:33 +0200

    tried header using Flex

---
+++ src/cmps/Header.jsx
@@ -0,0 +1,25 @@
+export function Header() {
+
+    return(
+        <>
+            <div className="header-container">
+                <span className="logo">Blogin</span>
+
+                <div className="nav-items">
+                    <div className="nav-item journal">Journal</div>
+                    <div className="nav-item about">About</div>
+                    <div className="nav-item work">Work</div>
+                    <div className="nav-item contact">Contact</div>
+
+                </div>
+
+
+
+            </div>
+
+
+
+
+        </>
+    )
+}
